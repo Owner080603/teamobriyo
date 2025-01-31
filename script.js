96e46dafebfb7de589c121cd0795aa7fea26c64b
Diego Cortijo <diegoandrecortijomejia@gmail.com>
2025-01-31 13:00:37 -0500

    corregido nuevamente

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarSegundaPantalla() {
+    document.getElementById("pantalla1").classList.add("oculto");
+    document.getElementById("pantalla2").classList.remove("oculto");
+}
+
+function mostrarPantallaFinal() {
+    document.getElementById("pantalla2").classList.add("oculto");
+    document.getElementById("pantallaFinal").classList.remove("oculto");
+}
